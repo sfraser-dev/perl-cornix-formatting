8187700bbca96a10e035a4c3544c2ab20932b824
Banana Man <jack01110111@yahoo.co.uk>
2022-09-03 20:17:39 +0100

    Fleshing-out reading the command line args. Adding in my Cornix bot client names. Beginning to mimic the Cornix template.

--- cornix-entries.pl
+++ cornix-entries.pl
@@ -2,43 +2,141 @@
 use warnings;
 use strict;
 use feature qw(say);
-use Getopt::Long;
+use Getopt::Long; # for processing command line args
+use File::Basename;
 
-# save arguments following -h or --host in the scalar $host
-# the '=s' means that an argument follows the option
-# they can follow by a space or '=' ( --host=127.0.0.1 )
-# same for --user or -u
-# same for --pass or -p
-GetOptions( 'numberOfEntries=s' => \my $numberOfEntries,
-			'highEntry=s' => \my $highEntry,
-            'lowEntry=s' => \my $lowEntry  
-          );
+########## Client: 
+my $client01 = "BM BinFuts (main)";
+my $client02 = "BM BinSpot (main)";
+my $client03 = "BM BybitKB7 Contract InvUSD (main) 260321";
+my $client04 = "BM BybitKB7 Contract LinUSDT (main) 211128";
+my $client05 = "SF BinFuts (main)";
+my $client06 = "SF BinSpot (main)";
+my $client07 = "SF Bybit Contract InvUSD (main) 210318";
+my $client08 = "BM BybitKB7 Contract LinUSDT (main) 281121";
+my $client09 = "SF FtxFuturesPerp (main)";
+my $client10 = "SF FtxFSpot (main)";
+my $client11 = "SF KucoinSpot (main)";
+########## Trade Type: 
+my $tradeTypeLong = "Regular (Long)";
+my $tradeTypeShort = "Regular (Short)";
+########## Leverage: 
+#my $levIso = "Isolated"; ## Leverage: Isolated (4.0X)!!!!!
+my $levCross = "Cross";  ## Leverage: Cross (4.0X)!!!!!
+########## Trailing: 
+my $trailingLine01 = "Trailing Configuration:";
+my $trailingLine02 = "Entry: Percentage (0.0%)";
+my $trailingLine03 = "Take-Profit: Percentage (0.0%)";
+my $trailingLine04 = "Stop: Breakeven -\n Trigger: Target(1)";
+
+
+my $script_name = basename($0);
+my $usage = sprintf("usage is: %s -n NoEntries -h HighEntry -l LowEntry -p CoinPair -c Client -t TradeType -v Leverage",$script_name); 
+my $numberOfEntries;
+my $highEntry;
+my $lowEntry;
+my $pair;
+my $clientIn;
+my $clientSelected;
+my $tradeTypeIn;
+my $tradeTypeSelected;
+my $leverage;
+my %args;
+GetOptions( \%args,
+			'n=s', # number of entries
+			'h=s', # highest entry value
+            'l=s', # lowest entry value
+			'p=s', # coin pair
+			'c=s', # client
+			't=s', # trade type
+			'v=s'  # leverage
+          ) or die "Invalid command line arguments!";
+die "Missing -n!\n".$usage unless $args{n};
+die "Missing -h!\n".$usage unless $args{h};
+die "Missing -l!\n".$usage unless $args{l};
+die "Missing -p!\n".$usage unless $args{p};
+die "Missing -c!\n".$usage unless $args{c};
+die "Missing -t!\n".$usage unless $args{t};
+die "Missing -v!\n".$usage unless $args{v};
+
+$numberOfEntries = $args{n};
+$highEntry = $args{h};
+$lowEntry = $args{l};
+$pair = $args{p};
+$clientIn = $args{c};
+$tradeTypeIn = $args{t};
+$leverage = $args{v};
+
+if ($numberOfEntries <= 2) { die "\nerror: numberOfEntries should be > 2\n".$usage; }
+if ($highEntry <= $lowEntry) { die "\nerror: highEntry is <= lowEntry\n".$usage; }
+if ($clientIn == 1) {
+	$clientSelected = $client01;
+} elsif ($clientIn == 2) {
+	$clientSelected = $client02;
+} elsif ($clientIn == 3) {
+	$clientSelected = $client03;
+} elsif ($clientIn == 4) {
+	$clientSelected = $client04;
+} elsif ($clientIn == 5) {
+	$clientSelected = $client05;
+} elsif ($clientIn == 6) {
+	$clientSelected = $client06;
+} elsif ($clientIn == 7) {
+	$clientSelected = $client07;
+} elsif ($clientIn == 8) {
+	$clientSelected = $client08;
+} elsif ($clientIn == 9) {
+	$clientSelected = $client09;
+} elsif ($clientIn == 10) {
+	$clientSelected = $client10;
+} elsif ($clientIn == 11) {
+	$clientSelected = $client11;
+} else {
+	die "error: unknown client number";
+}
+# cannot read "0" from command line, use "-1" for no leverage
+if (($leverage<-1) or ($leverage >20)) { 
+	die "error: incorrect leverage (-1 <= lev <=20)";
+} else {
+	$levCross  = $leverage;
+}
+if ($tradeTypeIn eq "long") {
+	$tradeTypeSelected = $tradeTypeLong;
+} elsif ($tradeTypeIn eq "short") {
+	$tradeTypeSelected = $tradeTypeShort;
+} else {
+	die "error: TradeType must be 'long' or 'short'";
+}
 
 my $i=0;
 my $thediff =0;
 my $theincrement=0;
 my $newval=0;
-
-say $numberOfEntries;
-say $highEntry;
-say $lowEntry;
-
-if ($highEntry <= $lowEntry){
-	say "error, highEntry (-h) is less than lowEntry -(l)";
-	exit();
-}
-if ($numberOfEntries <= 2){
-	say "error, numberOfEntries (-n) should be greater than 2";
-	exit();
-}
-
 $thediff = $highEntry - $lowEntry;
-say $thediff;
-
+say "The difference is: ".$thediff;
 $theincrement = $thediff / $numberOfEntries;
-say $theincrement;
-
+say "The increment is: ".$theincrement;
 for($i=0; $i<10; $i++){
 	#$newval = sprintf("%.3f",$i);
 	printf("%.3f ", $i);
 }
+printf("\n");
+
+say"";
+say"";
+say"";
+say $pair;
+say "Client: ".$clientSelected;
+say "Trade Type: ".$tradeTypeSelected;
+if ($levCross >= 1) { say "Leverage: Cross (".$leverage.".0X)"; }
+say"";
+say "Entry Targets:";
+say"";
+say "Take-Profit Targets:";
+say"";
+say "Stop Targets:";
+say"";
+say $trailingLine01;
+say $trailingLine02;
+say $trailingLine03;
+say $trailingLine04;
