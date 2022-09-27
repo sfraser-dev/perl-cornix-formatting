08a0bbf66e73a9ea20279faacc7af841176f6e7d
Banana Man <jack01110111@yahoo.co.uk>
2022-09-27 15:04:21 +0100

    Adding function (HeavyWeightingAtEntryOrStoploss) to weight the entry percentages more at either entry or stop-loss

--- cornix-template-file-parse.pl
+++ cornix-template-file-parse.pl
@@ -5,6 +5,7 @@ use feature qw(say);
 use Getopt::Long; # for processing command line args
 use File::Basename;
 use POSIX qw(strftime);
+use List::Util qw(pairs);
 
 ########## subroutines
 sub createOutputFileName {
@@ -117,14 +118,17 @@ sub riskSofteningMultiplier {
 	# assumes advanced template entries are in the correct order (whether long or shorting)
 	my @strArr=@{$_[0]}; 					# dereference the passed array
 	my $stopLoss=$_[1];
+	
 	my @splitter;
 	my $entryPrice;
 	my $firstEntryPrice = 0;
 	my $percentage;
+	
 	my $arbitraryPositionValue = 10000;		# copy how calculated on spreadsheet
 	my $amountSpentAtThisEntry;
 	my $noCoinsObtainedAtThisEntry;
 	my $totalCoinsObtained=0;
+	
 	my $riskPercentageBasedOnEntry1;
 	my $avgEntryPrice;
 	my $riskPercentageBasedOnAvgEntry;
@@ -154,6 +158,101 @@ sub riskSofteningMultiplier {
 	return $riskSoftMult;
 }
 
+sub HeavyWeightingAtEntryOrStoploss {
+	my $entriesOrTargetsStr=$_[0];
+	my $noOfEntries=$_[1];
+	my $high=$_[2];
+	my $low=$_[3];
+	my $tradeTypeIn=$_[4];
+	my @strArr;
+	
+	# run EvenDistribution calculation first
+	@strArr = EvenDistribution($entriesOrTargetsStr,$noOfEntries,$high,$low,$tradeTypeIn);
+	
+	# get the percentages from @strArr
+	my @percentages;
+	for my $i (0 .. $#strArr) {
+		my @splitter=split / /, $strArr[$i];	# split line using spaces, [0]=1), [1]=value, [2]=hyphen, [3]=percentage
+		my $percentage = ($splitter[3]);
+		$percentage =~ s/%//g;				# remove percentage sign
+		push (@percentages, $percentage);
+	}
+	
+	# is there an odd or even number of percentages?
+	my $arrLengthPerc = @percentages;
+	my $mod = $arrLengthPerc % 2;
+	my $quot = int($arrLengthPerc / 2);
+	my $isEven;
+	if ($mod == 0) {
+		$isEven = 1;
+	} elsif ($mod == 1) {
+		$isEven = 0;
+	} else {
+		die "error: modulus calculation error";
+	}
+	
+	# create an array of percentage "pairs" (next to each other in the array)
+	my @temp;						
+	for(my $i = 0; $i < (int($arrLengthPerc/2)); $i++){
+		push(@temp, $i);
+		push(@temp, ($arrLengthPerc-1-$i));
+	}
+	# convert this array into a "pairs value" index array
+	my @indexPairs = pairs @temp;
+	
+	### array indexes, length 6 (even length)
+	# 00 01 02 03 04 05
+	### values
+	# 10 10 10 10 10 10		initial array values 
+	# 12 12 12 08 08 08		iteration 1 (weighting factor 2)
+	# 14 14 12 08 06 06		iteration 2 (weighting factor 2)
+	# 16 14 12 08 06 04		iteration 3 (weighting factor 2)
+	### index pairs
+	# 0,5 (0...length-1)
+	# 1,4 (1...length-2)
+	# 2,3 (2...length-3)
+	#########################################
+	### array indexes, length 5 (odd length)
+	# 00 01 02 03 04 
+	### values
+	# 10 10 10 10 10		initial array values 
+	# 12 12 10 08 08		iteration 1 (weighting factor 2)
+	# 14 12 10 08 06		iteration 2 (weighting factor 2)
+	### index pairs
+	# 0,4 (0...length-1)
+	# 1,3 (1...length-2)
+	my $weightingFactor = 0.25;
+	my $isEntryHeavyWeighting = 0;	# entry or stop-loss heavy weighting
+	for(my $x = 0; $x < (int($arrLengthPerc/2)); $x++){
+		for(my $i = 0; $i < (int($arrLengthPerc/2))-$x; $i++){
+			# my $ii = $indexPairs[$i]->key;
+			my $p = $indexPairs[$i]->value;
+			if ($isEntryHeavyWeighting == 1) {
+				$percentages[$i]+=$weightingFactor;
+				$percentages[$p]-=$weightingFactor;
+			} elsif ($isEntryHeavyWeighting == 0) {
+				$percentages[$i]-=$weightingFactor;
+				$percentages[$p]+=$weightingFactor;	
+			} else { die "error: isEntryHeavyWeighting incorrectly set"; }
+		}
+	}
+
+	# update strArr with the new weighted percentages
+	my @strArrNewPercentages;
+	for my $i (0 .. $#strArr) {
+		my @splitter=split / /, $strArr[$i];	# split line using spaces, [0]=1), [1]=value, [2]=hyphen, [3]=percentage
+		my $num = ($splitter[0]);
+		my $val = ($splitter[1]);
+		my $hash = ($splitter[2]);
+		my $per = ($splitter[3]);
+		$per = sprintf("%.2f", $percentages[$i]);
+		my $newline = $num." ".$val." ".$hash." "."$per%\n";
+		push(@strArrNewPercentages, $newline);
+	}
+			
+	return @strArrNewPercentages;
+}
+
 sub createAdvancedTemplate {
 	my $pair = $_[0];
 	my $clientSelected = $_[1];
@@ -181,7 +280,8 @@ sub createAdvancedTemplate {
 	
 	push (@template,"\n");
 	push (@template,"Entry Targets:\n");
-	@strArr = EvenDistribution("entries",$noOfEntries,$highEntry,$lowEntry,$tradeTypeIn);
+	#@strArr = EvenDistribution("entries",$noOfEntries,$highEntry,$lowEntry,$tradeTypeIn);
+	@strArr = HeavyWeightingAtEntryOrStoploss("entries",$noOfEntries,$highEntry,$lowEntry,$tradeTypeIn);
 	foreach $strRead (@strArr) {
 		push(@template,$strRead);
 	}
@@ -190,6 +290,7 @@ sub createAdvancedTemplate {
 	push (@template,"\n");
 	push (@template,"Take-Profit Targets:\n");
 	@strArr = EvenDistribution("targets",$noOfTargets,$highTarget,$lowTarget,$tradeTypeIn);
+	#@strArr = EntryHeavyWeighting("targets",$noOfTargets,$highTarget,$lowTarget,$tradeTypeIn);
 	foreach $strRead (@strArr) {
 		push(@template,$strRead);
 	}
